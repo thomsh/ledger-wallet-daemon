d77e72269b5a141379ad4476b78f01f1252c18c4
Thomsh <5822844+thomsh@users.noreply.github.com>
2018-10-09 10:56:37 +0200

    INFRA:332 Dockerize Wallet Daemon (#22)
    Using openjdk 8 (u181) should be change/update via the Dockerfile
    Using embeded libledger-core jar

---
+++ docker/install_run_deps.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+mkdir -p /app/database # for sqlite (can be mount outside the container at runlevel)
+
+# This man folder is required to be able to install packages, but it does not exist in debian:stretch-slim.
+# So we create it.
+mkdir -p /usr/share/man/man1
+apt-get update && apt-get -qy install openjdk-8-jdk=${JDK_DEBIAN_VERSION}
+
+# Cleanup
+apt-get clean
+rm -rf -- /var/lib/apt/lists/*
+exit 0
