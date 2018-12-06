7847a16d72af7d0349568fb31a21208c10094729
Thomsh <5822844+thomsh@users.noreply.github.com>
2018-12-06 18:47:55 +0100

    INFRA-425: Add debugging tools in docker image (#59)

--- docker/install_run_deps.sh
+++ docker/install_run_deps.sh
@@ -8,6 +8,9 @@ mkdir -p /app/database # for sqlite (can be mount outside the container at runle
 mkdir -p /usr/share/man/man1
 apt-get update && apt-get -qy install openjdk-8-jdk=${JDK_DEBIAN_VERSION}
 
+# Debug tools untils we have our ledger-stretch-slim image
+apt-get install -yq curl netcat iputils-ping iproute2 lsof procps
+
 # Cleanup
 apt-get clean
 rm -rf -- /var/lib/apt/lists/*
