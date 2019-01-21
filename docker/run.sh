60c73eb11b3b438a422cdcab4fbcec58591bca10
thomsh <thomas.hebras@ledger.fr>
2019-01-21 20:21:04 +0100

    INFRA-492: Support proxy for websocket

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-set -eux
-exec ./bin/daemon -http.port=":$HTTP_PORT" -admin.port=":$ADMIN_PORT" $@
+set -euxo pipefail
+
+OPTS="-http.port=:${HTTP_PORT} -admin.port=:${ADMIN_PORT}"
+
+if [ -n "${WALLET_PROXY_ENABLED+x}" ] && [ "${WALLET_PROXY_ENABLED}" = "true" ];then
+  OPTS="${OPTS} -Dhttp.proxyHost=${WALLET_PROXY_HOST} -Dhttp.proxyPort=${WALLET_PROXY_PORT}"
+fi
+exec ./bin/daemon ${OPTS} $@
